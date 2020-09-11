0f79090e43c7bef8fe7afdb22c4359909e69377c
Basil Homadi <deviltearbasil@gmail.com>
2020-09-11 15:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 // Write your solution in this file!
 let driver = {}
+
+function updateDriverWithKeyAndValue() {
+  
+}
+
+function destructivelyUpdateDriverWithKeyAndValue() {
+  
+}
+
+function deleteFromDriverByKey() {
+  
+}
+
+function destructivelyDeleteFromDriverByKey() {
+  
+}
